81c8b03eef09b31b5f54d9c3fea8401b3b9cd336
jyukipann <jyukipann@yahoo.co.jp>
2024-08-26 14:57:15 +0900

    バックスペースのミスを修正

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/via/keymap.c
@@ -42,7 +42,7 @@ user_config_t user_config;
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT_universal(
 		KC_ESC   , KC_1     , KC_2     , KC_3     , KC_4     , KC_5     ,                                  KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , KC_DEL   ,
-		KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                  KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_BS    ,
+		KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                  KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_BSPC  ,
 		KC_LCTL  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                  KC_H     , KC_J     , KC_K     , KC_L     ,LT(3,KC_ENT),KC_RCTL ,
 		KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     , KC_LNG2  ,              KC_LNG1, KC_N     , KC_M     , KC_BTN1  , KC_BTN2  , KC_BTN3  , KC_RSFT  ,
 		KC_LCTL  , KC_LALT  , KC_LWIN  , KC_LALT  ,LT(1,KC_SPC),LT(2,KC_TAB),LT(3,KC_ESC),        KC_BSPC,LT(2,KC_ENT), _______, _______  , _______  , G(KC_DOT), LT(2,KC_RWIN)
