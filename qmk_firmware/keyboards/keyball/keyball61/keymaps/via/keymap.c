c9c613176629e062e6221e8ed7d303df7a7371db
jyukipann <jyukipann@yahoo.co.jp>
2025-01-10 23:45:42 +0900

    一旦完成形

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/via/keymap.c
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	), 
 
 	[2] = LAYOUT_universal(
-		_______ , KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_F5    ,                                            KC_F6      , KC_F7      , KC_F8   , KC_F9   , KC_F10  , KC_F11  ,
+		QK_BOOT , KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_F5    ,                                            KC_F6      , KC_F7      , KC_F8   , KC_F9   , KC_F10  , KC_F11  ,
 		RGB_TOG , KBC_RST  , KBC_SAVE , CPI_D100 , CPI_I100 , AML_TO   ,                                            KC_PGUP    , KC_LEFT    , KC_UP   , KC_RGHT , _______ , KC_F12  ,
 		RGB_VAI , RGB_HUI  , RGB_SAI  , SSNP_FRE , SSNP_VRT , SSNP_HOR ,                                            KC_PGDN    , KC_BTN1    , KC_DOWN , KC_BTN2 , KC_BTN3 , _______ ,
 		RGB_VAD , RGB_HUD  , RGB_SAD  , _______  , OLED_MIN , OLED_MAX , TOGGLE_OLED ,                TOGGLE_OLED , A(KC_LEFT) , A(KC_RGHT) , KC_BTN1 , KC_BTN2 , KC_BTN3 , _______ ,
